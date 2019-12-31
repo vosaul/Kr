867b153b6a3d20b23bac7086d5c51ec5cafaac9f
vosaul <fullstack.dev321@gmail.com>
2019-12-31 15:16:22 +0200

    search

--- assets/tipuesearch/tipuesearch_content.js
+++ assets/tipuesearch/tipuesearch_content.js
@@ -5,7 +5,15 @@
 {%- if counter != 0 -%}
 var tipuesearch = {"pages": [
 {% for post in site.posts %}
-{ "title": "{{post.title}}", "tags": "{{post.tags}}", "text": "text", "url": "{{post.url | prepend: site.url}}"}{%- if index < counter -%},{% endif %}
+{%- assign text = post.content | 
+  strip_newlines |
+  replace: '/', " " | 
+  replace: '=', " " | 
+  remove: '"' | 
+  remove: '>' | 
+  remove: ':' | 
+  remove: "<" -%}
+{ "title": "{{post.title}}", "tags": "{{post.tags}}", "text": "{{text}}", "url": "{{post.url | prepend: site.url}}"}{%- if index < counter -%},{% endif %}
 {%- assign index = index | plus: 1 -%}
 {% endfor %}
 ]};
